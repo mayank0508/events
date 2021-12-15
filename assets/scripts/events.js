e7b05988994eb6096cc677d1fad88fa2d7e764d0
mayank0508 <thakurmayank0508@gmail.com>
2021-12-15 18:05:36 +0530

    remove event listner

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,7 +5,13 @@ const button = document.querySelector('button');
 // } // this is one way to handle click events
 
 const clickButton = () => {
-    alert('The Event Button is clicked')
-}
+  alert('The Event Button is clicked');
+};
 
 button.onclick = clickButton;
+
+button.addEventListener('click', clickButton);
+
+setTimeout(() => {
+  button.removeEventListener('click', clickButton);
+}, 2000);
