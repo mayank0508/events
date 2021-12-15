858beed2b58260d769c75cb443b7e0e0fc5fda71
mayank0508 <thakurmayank0508@gmail.com>
2021-12-15 17:49:00 +0530

    event button

---
+++ assets/scripts/events.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+
+// button.onclick = function() {
+
+// } // this is one way to handle click events
+
+const clickButton = () => {
+    alert('The Event Button is clicked')
+}``
+
+button.onclick = clickButton;
