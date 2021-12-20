c4fd479d94107d709f7209d74560cea51e58df69
mayank0508 <thakurmayank0508@gmail.com>
2021-12-20 13:59:30 +0530

    mouse enter event

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,7 +5,7 @@ const button = document.querySelectorAll('button');
 // } // this is one way to handle click events
 
 const clickButton = (event) => {
- // event.target.disabled = true; // this property is used to make sure that the same button is not clicked more than one time
+  event.target.disabled = true; // this property is used to make sure that the same button is not clicked more than one time
   console.log(event);
 };
 
@@ -13,15 +13,15 @@ const clickButton = (event) => {
 
 // button.addEventListener('click', clickButton);
 
-// button.forEach(button => {
-//   button.addEventListener('click', clickButton);
-// })
+button.forEach(button => {
+  button.addEventListener('click', clickButton);
+})
 
 // setTimeout(() => {  // this command is used to time out the particular elemtn after a while
 //   button.removeEventListener('click', clickButton);
 // }, 2000);
 
-button.forEach(button => { // this is a type of event handler which is triggered when you hover over a button 
-  button.addEventListener('mouseenter', clickButton);
-})
+// button.forEach(button => { // this is a type of event handler which is triggered when you hover over a button 
+//   button.addEventListener('mouseenter', clickButton);
+// })
 
